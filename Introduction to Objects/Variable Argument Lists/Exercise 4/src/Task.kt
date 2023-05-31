98bb494e1f8e06ee3de04740072ef8934efa2bfe
vvchn <vl.soldatov4@gmail.com>
2023-05-31 19:28:46 +0300

    Introduction to Objects - Variable Argument Lists (fixed)

--- Introduction to Objects/Variable Argument Lists/Exercise 4/src/Task.kt
+++ Introduction to Objects/Variable Argument Lists/Exercise 4/src/Task.kt
@@ -2,6 +2,11 @@
 package variableArgumentListsExercise4
 import atomictest.eq
 
+/** Much slower at runtime:
+1 – creating N times a new list with new strings
+2 – converting N sublists -> array -> string
+N + N  */
+
 fun flatten(listOfLists: List<List<String>>): List<String> {
   var res = listOf<String>()
   for (i in listOfLists) {
@@ -9,6 +14,19 @@ fun flatten(listOfLists: List<List<String>>): List<String> {
   }
   return res
 }
+
+/** ANSWER
+val result = mutableListOf<String>()
+for (list in listOfLists) {
+result.addAll(list)
+}
+return result
+
+Much faster at runtime
+1 – Only one creation of mutable list
+2 – Fast adding collection (list) element to collection (list)
+3 – Only one fast conversion from mutable to immutable list (without reinitialization ones)
+1 + N + 1 */
 
 fun main() {
   val listOfLists = listOf(listOf("a", "b"), listOf("c", "d"))
